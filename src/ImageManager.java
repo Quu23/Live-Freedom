48da0ed1a6f532096d42f39951037ce6800b5344
Quu <91326783+Quu23@users.noreply.github.com>
2022-10-10 21:22:13 +0900

    javadocコメントの追加

--- src/ImageManager.java
+++ src/ImageManager.java
@@ -8,18 +8,31 @@ import java.awt.Toolkit;
  * ゲーム内の様々な画像を管理するメゾット群
  */
 public class ImageManager {
-
+    /**
+     * 画像を格納するリスト
+     */
     private static List<Image> images = new ArrayList<>();
 
+    /**
+     * 指定されたパスの画像を読み込み格納する
+     */
     public static void imageLoad(String path) {
        images.add(Toolkit.getDefaultToolkit().getImage(path));
     }
+    /**
+     * 指定されたパス(複数)の画像を読み込み格納する
+     */
     public static void imageLoad(String[] paths) {
         for (String path : paths) {
             images.add(Toolkit.getDefaultToolkit().getImage(path));
         }
     }
+    /**
+     * 格納された順番を指定してその時の画像を返す
+     */
     public static Image getImage(int index) {
         return images.get(index);
     }
+
+    private ImageManager(){}
 }
